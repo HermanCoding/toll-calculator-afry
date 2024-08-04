06f619d746975049d66d8ba2bfe026655c6a5546
HermaN <d.herman85@gmail.com>
2024-08-04 15:34:10 +0200

    Working program to existing code

--- TollFeeCalculator/Program.cs
+++ TollFeeCalculator/Program.cs
@@ -1,6 +1,27 @@
-﻿namespace TollFeeCalculator
+﻿using System;
+
+namespace TollFeeCalculator
 {
     internal class Program
     {
+        static void Main(string[] args)
+        {
+            Vehicle car = new Car();
+            Vehicle motorbike = new Motorbike();
+
+            DateTime[] dates = {
+            new DateTime(2023, 8, 4, 6, 0, 0),
+            new DateTime(2023, 8, 4, 6, 30, 0),
+            new DateTime(2023, 8, 4, 7, 30, 0)
+        };
+
+            TollCalculator calculator = new TollCalculator();
+
+            int tollFeeCar = calculator.GetTollFee(car, dates);
+            Console.WriteLine($"The total toll fee for the car is: {tollFeeCar}");
+
+            int tollFeeMotorbike = calculator.GetTollFee(motorbike, dates);
+            Console.WriteLine($"The total toll fee for the motorbike is: {tollFeeMotorbike}");
+        }
     }
 }
