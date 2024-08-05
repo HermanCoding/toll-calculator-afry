c62168f872ac9292bb58a64ed3a05ab60fced777
HermaN <d.herman85@gmail.com>
2024-08-05 20:00:36 +0200

    Add one GetTollFeeTest

--- TollFeeCalculator/Vehicle.cs
+++ TollFeeCalculator/Vehicle.cs
@@ -6,5 +6,7 @@ namespace TollFeeCalculator
     {
         String VehicleType();
         Boolean TollFreeVehicles();
+        // String RegistrationNumber();  // Bör kanske hämta en registreringsnummer så man kan göra varje bil unik?
+        // DateTime HigestExpanseWithinTheHoue(); //  Kanske skall implementera detta.
     }
 }
--- TollFeeCalculatorTest/TollCalculatorTest.cs
+++ TollFeeCalculatorTest/TollCalculatorTest.cs
@@ -9,7 +9,32 @@ namespace TollFeeCalculatorTest
         }
 
         [Fact]
-        public void IsTollFreeDateTest_ShouldReturnTrues()
+        public void GetTollFeeTest_ShouldReturn24()
+        {
+            // Arange
+            TollCalculator tollCalc = new TollCalculator();
+            Vehicle car = new Car();
+            DateTime[] dates = {
+                new DateTime(2023, 8, 4, 6, 0, 0),
+                new DateTime(2023, 8, 4, 13, 30, 0),
+                new DateTime(2023, 8, 4, 18, 00, 0)
+            };
+
+            int expectedTotalFee = 24;
+            int actualTotalFee = 0;
+
+            // Act
+            foreach (var date in dates)
+            {
+                actualTotalFee += tollCalc.GetTollFee(date, car);
+            }
+
+            // Assert
+            Assert.Equal(expectedTotalFee, actualTotalFee);
+        }
+
+        [Fact]
+        public void IsTollFreeDateTest_ShouldReturnTrue()
         {
             DateTime date = new DateTime(2023,08,13);
 
